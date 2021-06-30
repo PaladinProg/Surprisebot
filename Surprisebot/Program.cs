cd880eec61d760e8ea09b16e3426091d2298fe41
Ivan <ivanlukyanov12@gmail.com>
2021-06-30 13:20:19 +0300

    Добавьте файлы проекта.

---
+++ Surprisebot/Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Telegram.Bot;
+using Telegram.Bot.Args;
+using Telegram.Bot.Types.Enums;
+using Telegram.Bot.Types.ReplyMarkups;
+//1823203185:AAHYgw1bZMLe6ZAoDYxfFYnC9HI9lvHEg38
+namespace Surprisebot
+{
+    class Program
+    {
+        private const string TEXT_1 = "Задание 1";
+        private const string TEXT_2 = "Задание 2";
+        private const string TEXT_3 = "Задание 3";
+        private const string TEXT_4 = "Конец";
+        /*private const string TEXT_5 = "";
+        private const string TEXT_6 = "Нашла (Третье задание)";
+        private const string TEXT_7 = "Прислала (Третье задание)";
+        private const string TEXT_8 = "Ссылка";*/
+
+        static ITelegramBotClient botclient;//обьявление телеграм клиента
+        static void Main(string[] args)
+        {
+            try
+            {
+                botclient = new TelegramBotClient("1823203185:AAHYgw1bZMLe6ZAoDYxfFYnC9HI9lvHEg38") { Timeout = TimeSpan.FromSeconds(10) };//таймаут нужен чтобы снизить нагрузку бота на телеграм, 
+                                                                                                                                           //если не поставить этот параметр то бот будет создавать большую нагрузку и может быть заблокирован
+                var me = botclient.GetMeAsync().Result;
+                Console.WriteLine($"Bot ID: {me.Id} Bot Name: {me.FirstName}");//проверка работоспособости бота
+
+                botclient.OnMessage += Bot_OnMessage; //добавляем метод в котором будет функционал
+                botclient.StartReceiving();
+
+                Console.ReadKey();
+                botclient.StopReceiving();
+            }
+            catch (Exception ex) { }
+        }
+
+        public static async void Bot_OnMessage(object sender, MessageEventArgs e)//используем асинхронный метод чтобы поток данных не блокировался во время его получения
+        {
+            var text = e?.Message?.Text;//получение текста от пользователя
+            var message = e.Message;
+            string name = $"{message.From.FirstName} {message.From.LastName}";//получение имени пользователя
+            if (message.Type == MessageType.Text)
+            {//проверка на тип получаемого сообщения, он должен быть текстовым
+
+                Console.WriteLine($"Поступила инормация: '{text}' из чата: '{e.Message.Chat.Id}' от пользователя: {name}");//проверка того что сообщения доходят до бота
+
+                switch (message.Text)
+                {
+                    case "/start"://команда старта бота(запускается автоматичекски при первом обращении к боту)
+                        await botclient.SendTextMessageAsync(
+                            chatId: e.Message.Chat,
+                            text: $"Сыграем в игру?", replyMarkup: GetButtons()
+                        ).ConfigureAwait(false);
+                        break;
+                    case TEXT_1:
+                        botclient.SendTextMessageAsync(e.Message.Chat.Id, "Первое задание: Найдт QR код и отсканируй. Вот координаты 59.984029, 30.192480. Затем пришли двоичный код с сайта и приступай ко второму заданию", replyMarkup: GetButtons());
+                        break;
+                    case TEXT_2:
+                        botclient.SendTextMessageAsync(e.Message.Chat.Id, "Второе задание: Найди QR код и отсканируй. Вот координаты …  . Пришли двоичный код с сайта и приступай к третьему заданию", replyMarkup: GetButtons());
+                        break;
+                    case TEXT_3:
+                        botclient.SendTextMessageAsync(e.Message.Chat.Id, "Третье задание: Найди QR код и отсканируй. Вот координаты … .Пришли двоичный код с сайта и жми на кнопку конец", replyMarkup: GetButtons());
+                        break;
+                    case TEXT_4:
+                        botclient.SendTextMessageAsync(e.Message.Chat.Id, "Поздравляю! Ты прошла все задания. Теперь перейди по ссылке (ссылка) и переведи весь двоичный код в текст", replyMarkup: GetButtons());
+                        break;
+                    default:
+                        botclient.SendTextMessageAsync(e.Message.Chat.Id, "Пожалуйста следуй инструкции", replyMarkup: GetButtons());
+                        break;
+                }
+            }
+        }
+
+        private static IReplyMarkup GetButtons()
+        { 
+            return new ReplyKeyboardMarkup
+            {
+                Keyboard = new List<List<KeyboardButton>>
+                {
+                    new List<KeyboardButton>{new KeyboardButton { Text = TEXT_1 }, },
+                         new List<KeyboardButton>{new KeyboardButton { Text = TEXT_2 }, },
+                            new List<KeyboardButton>{new KeyboardButton { Text = TEXT_3 }, },
+                                new List<KeyboardButton>{new KeyboardButton { Text = TEXT_4 }, },
+                        
+                }
+            };
+        }   
+    }
+}
---
+++ Surprisebot/TelegramBotHelper.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using Telegram.Bot.Types.ReplyMarkups;
+
+namespace Surprisebot
+{
+    internal class TelegramBotHelper
+    {
+        private const string TEXT_1 = "Задание 1";
+        private const string TEXT_2 = "Задание 2";
+        private const string TEXT_3 = "Задание 3";
+        private const string TEXT_4 = "Задание 4";
+        private string _token;
+        Telegram.Bot.TelegramBotClient _client;
+        public TelegramBotHelper(string token)
+        {
+            this._token = token;
+        }
+
+        internal void GetUpdates()
+        {
+            _client = new Telegram.Bot.TelegramBotClient(_token);
+            var me = _client.GetMeAsync().Result;
+            if (me != null && !string.IsNullOrEmpty(me.Username))
+            {
+                while (true)
+                {
+                    try
+                    {
+                        var updates = _client.GetUpdatesAsync().Result;
+                        if (updates != null && updates.Count() > 0)
+                        {
+                            foreach(var update in updates)
+                            {
+                                processUpdate(update);
+                            }
+                        }
+                    }
+                    catch(Exception ex) {   Console.WriteLine(ex.Message);  }
+
+                    Thread.Sleep(1000);
+                }
+            }
+        }
+
+        private void processUpdate(Telegram.Bot.Types.Update update)
+        {
+            switch (update.Type)
+            {
+                case Telegram.Bot.Types.Enums.UpdateType.Message:
+                    var text = update.Message.Text;
+                    switch (text)
+                    {
+                        case TEXT_1:
+                            _client.SendTextMessageAsync(update.Message.Chat.Id, "Текст задания 1", replyMarkup: GetButtons());
+                            break;
+                        case TEXT_2:
+                            _client.SendTextMessageAsync(update.Message.Chat.Id, "Текст задания 2", replyMarkup: GetButtons());
+                            break;
+                        case TEXT_3:
+                            _client.SendTextMessageAsync(update.Message.Chat.Id, "Текст зажания 3", replyMarkup: GetButtons());
+                            break;
+                        case TEXT_4:
+                            _client.SendTextMessageAsync(update.Message.Chat.Id, "Текст задания 4", replyMarkup: GetButtons());
+                            break;
+                    }
+                    break;
+                default:
+                    Console.WriteLine(update.Type + "не обрабатываемый тип сообщения");
+                    break;
+            }
+        }
+
+        private IReplyMarkup GetButtons()
+        {
+            return new ReplyKeyboardMarkup
+            {
+                Keyboard = new List<List<KeyboardButton>>
+                {
+                    new List<KeyboardButton>{new KeyboardButton { Text = TEXT_1 }, new KeyboardButton { Text = TEXT_2 }, },
+                        new List<KeyboardButton>{new KeyboardButton { Text = TEXT_3 }, new KeyboardButton { Text = TEXT_4 }, }
+                  
+                }
+            };
+        }
+    }
+}
